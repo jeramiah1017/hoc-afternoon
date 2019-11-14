bfda234dcec33a73f80c92b454176e21d8e0be15
jeramiah1017 <55998280+jeramiah1017@users.noreply.github.com>
2019-11-13 19:41:26 -0700

    finished

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+
+import CurrencyHOC from './components/CurrencyConverter'
 
 class App extends Component {
   render() {
     return (
       <div>
-
+        <h2>Higher Order Component</h2>
+        <CurrencyHOC />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
